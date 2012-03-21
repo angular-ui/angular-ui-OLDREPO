eb3f345c2d11bc162aea7bc6ebb9feb526ebd4d4
Dean Sofer <dean@jobvite.com>
2012-03-21 12:48:42 -0700

    Added scrollfix directive

---
+++ linky.js
@@ -0,0 +1,10 @@
+/**
+ * Adds a 'fixed' class to the element when the page scrolls past it's position.
+ *
+ * @param [offset] {int} optional Y-offset to override the detected offset
+ */
+angular.module('ui.directives', []).
+	directive('uiLinky', [function() {
+		return function(scope, elm, attrs) {
+		};
+	}]);
---
+++ scrollfix.js
@@ -0,0 +1,20 @@
+/**
+ * Adds a 'fixed' class to the element when the page scrolls past it's position.
+ *
+ * @param [offset] {int} optional Y-offset to override the detected offset
+ */
+angular.module('ui.directives', []).
+	directive('uiScrollfix', [function() {
+		return function(scope, elm, attrs) {
+			if (!attrs.jvScrollfix) {
+				attrs.jvScrollfix = elm.offset().top;
+			}
+			$(window).scroll(function(){
+				if (!elm.hasClass('fixed') && window.pageYOffset > attrs.jvScrollfix) {
+					elm.addClass('fixed');
+				} else if (elm.hasClass('fixed') && window.pageYOffset < attrs.jvScrollfix) {
+					elm.removeClass('fixed');
+				}
+			});
+		};
+	}]);
