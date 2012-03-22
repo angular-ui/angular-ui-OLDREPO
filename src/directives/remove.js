1c69bee9118d27f2d10700e30bf4e65665fb9f61
Dean Sofer <dean@jobvite.com>
2012-03-22 14:05:14 -0700

    Added ui-remove directive

---
+++ src/directives/remove.js
@@ -0,0 +1,17 @@
+
+/**
+ * ui-remove directive
+ * 
+ * Actually removes html from the DOM instead of hiding it for assistance with 
+ * CSS3 selectors such as :first-child, :last-child, etc
+ * 
+ * @param remove {boolean} condition to check if the element should be removed form the DOM
+ */
+
+angular.module('ui.directives', []).directive('uiRemove', [function() {
+	return function(scope, elm, attrs) {
+		if (scope.$eval(attrs.uiRemove)) {
+			elm.remove();
+		}
+	};
+}]);
