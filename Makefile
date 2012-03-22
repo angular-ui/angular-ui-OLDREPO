3e7c28f13b84558c835a392a3fe9fd7106c93455
Dean Sofer <proloser@hotmail.com>
2012-03-22 00:54:49 -0700

    Added showhide directives for better css control

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ JS_SRC_DIR = src/
 JS_LIB_DIR = lib/
 
 JS_FILES = \
-	${JS_SRC_DIR}directives/linky.js\
-	${JS_SRC_DIR}directives/scrollfix.js\
-	${JS_SRC_DIR}filters/highlight.js\
+	${JS_SRC_DIR}directives/*\
+	${JS_SRC_DIR}filters/*\
 	${JS_SRC_DIR}module.js
 
 js:
---
+++ src/directives/showhide.js
@@ -0,0 +1,62 @@
+
+/**
+ * NOTE: Only adds classes, you must add the class definition yourself
+ */
+
+/**
+ * uiShow Directive
+ *
+ * Adds a 'show' class to the element instead of display:block
+ * Created to allow tighter control  of CSS without bulkier directives
+ *
+ * @param expression {boolean} evaluated expression to determine if the class should be added
+ */
+
+angular.module('ui.directives', []).directive('uiShow', [function() {
+	return function(scope, elm, attrs) {
+		if (scope.$eval(attrs.uiShow)) {
+			elm.addClass('show');
+		} else {
+			elm.removeClass('show');
+		}
+	};
+}]);
+
+/**
+ * uiHide Directive
+ *
+ * Adds a 'hide' class to the element instead of display:block
+ * Created to allow tighter control  of CSS without bulkier directives
+ *
+ * @param expression {boolean} evaluated expression to determine if the class should be added
+ */
+
+angular.module('ui.directives', []).directive('uiHide', [function() {
+	return function(scope, elm, attrs) {
+		if (scope.$eval(attrs.uiShow)) {
+			elm.addClass('hide');
+		} else {
+			elm.removeClass('hide');
+		}
+	};
+}]);
+
+/**
+ * uiToggle Directive
+ *
+ * Adds a class 'show' if true, and a 'hide' if false to the element instead of display:block/display:none
+ * Created to allow tighter control  of CSS without bulkier directives. This also allows you to override the
+ * default visibility of the element using either class.
+ *
+ * @param expression {boolean} evaluated expression to determine if the class should be added
+ */
+
+angular.module('ui.directives', []).directive('uiToggle', [function() {
+	return function(scope, elm, attrs) {
+		if (scope.$eval(attrs.uiShow)) {
+			elm.switchClass('show', 'hide');
+		} else {
+			elm.removeClass('hide', 'show');
+		}
+	};
+}]);
