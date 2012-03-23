769c550337d2510c8fbeca1361f5aa53034429cb
Dean Sofer <dean@jobvite.com>
2012-03-23 15:57:11 -0700

    Added a length filter

---
+++ src/js/filters/length.js
@@ -0,0 +1,10 @@
+
+/**
+ * Returns the length property of the filtered object
+ */
+ 
+angular.module('ui.filters', []).filter('length', function() {
+	return function(value) {
+		return value.length;
+	};
+});
