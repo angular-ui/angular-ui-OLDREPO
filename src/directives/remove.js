d2f3bc5799b03e6ea8255309bc575bc64c08d2e2
Dean Sofer <dean@jobvite.com>
2012-03-22 18:27:13 -0700

    Fixed ui-remove

--- src/directives/remove.js
+++ src/directives/remove.js
@@ -3,13 +3,31 @@
  * Actually removes html from the DOM instead of hiding it for assistance with 
  * CSS3 selectors such as :first-child, :last-child, etc
  * 
+ * NOTE: This solution may not behave perfectly when used with or around other directives that also
+ *   manipulate the dom.
+ * 
+ * @todo Add a more resilient solution to injecting removed elements back into the DOM (instead of relying on nextElm)
  * @param remove {boolean} condition to check if the element should be removed form the DOM
  */
 
 angular.module('ui.directives', []).directive('uiRemove', [function() {
 	return function(scope, elm, attrs) {
-		if (scope.$eval(attrs.uiRemove)) {
-			elm.remove();
-		}
+		var remove, nextElm;
+		scope.$watch(attrs.uiRemove, function(scope, newValue, oldValue) {
+			if (newValue) {
+				nextElm = elm.next();
+				// Checks for jQuery lib usage
+				if (elm['detach']) {
+					elm.detach();
+				} else {
+					elm.remove();
+				}    
+			} else {
+				if (nextElm) {
+					nextElm.before(elm);
+					nextElm = false;            
+				}
+			}
+		});
 	};
 }]);
