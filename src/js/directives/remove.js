3559ae21431ac83c558e54dec338704c874bdf45
Dean Sofer <proloser@hotmail.com>
2012-04-08 00:01:47 -0700

    Improved the remove directive to do better position handling

--- src/js/directives/remove.js
+++ src/js/directives/remove.js
@@ -12,20 +12,27 @@
 
 angular.module('ui.directives').directive('uiRemove', [function() {
 	return function(scope, elm, attrs) {
-		var remove, nextElm;
-		scope.$watch(attrs.uiRemove, function(newVal, oldVal) {
-			if (newVal) {
-				nextElm = elm.next();
-				// Checks for jQuery lib usage
-				if (elm['detach']) {
-					elm.detach();
+		var parent = elm.parent();
+		var expression = attrs.jvDetach;
+		elm.data('ui-remove-index', elm.index());
+		scope.$watch(expression, function(newValue, oldvalue) {
+			var index, children, child;
+			if (newValue) {
+				elm.detach(); 
+			} else if (!$.contains(parent, elm)) {
+				index = elm.data('ui-remove-index');
+				children = elm.parent().children();
+				if (children.length > 0) {
+					for (var i = 0; i < children.length; i++) {
+						child = children[i];
+						if (index > child.index() && i === children.length-1) {
+							child.after(elm);
+						} else {
+							child.before(elm);
+						}
+					}
 				} else {
-					elm.remove();
-				}    
-			} else {
-				if (nextElm) {
-					nextElm.before(elm);
-					nextElm = false;            
+					parent.append(elm);
 				}
 			}
 		});
