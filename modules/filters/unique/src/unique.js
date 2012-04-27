a33efac5f67f8d21be23180e1baf104d1b51896f
Dean Sofer <dean@jobvite.com>
2012-04-26 17:37:28 -0700

    Added the ability to check unique of a flat array

--- modules/filters/unique/src/unique.js
+++ modules/filters/unique/src/unique.js
@@ -6,14 +6,28 @@
  */
 angular.module('ui.filters').filter('unique', function() {
 	return function(items, key) {
-		var hashCheck = {};
-		for (i in items) {
-			var value = items[i][key];
-			if (typeof(hashCheck[value]) !== 'undefined') {
-				delete items[i];
-			} else {
-				hashCheck[value] = true;
+		if (key) {
+			var hashCheck = {};
+			for (i in items) {
+				var value = items[i][key];
+				if (typeof(hashCheck[value]) !== 'undefined') {
+					delete items[i];
+				} else {
+					hashCheck[value] = true;
+				}
 			}
+		} else {
+			var o = {},
+				i, 
+				l = items.length, 
+				r = [];
+			for (i=0; i<l;i+=1) {
+				o[items[i]] = items[i];
+			}
+			for (i in o) {
+				r.push(o[i]);
+			}
+			items = r;
 		}
 		return items;
 	};
