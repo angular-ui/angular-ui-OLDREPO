2af5472f6d84a6135295fabf2324b4d5a2dcb240
Dean Sofer <dean@jobvite.com>
2012-05-17 15:08:24 -0700

    Enhanced the directive for ui-event to be more versatile

--- build/angular-ui.js
+++ build/angular-ui.js
@@ -88,16 +88,28 @@ angular.module('ui', [
 
 /**
  * General-purpose Event binding. Bind any event not natively supported by Angular
- * Both ui-event-call is a required attribute.
+ * Both ui-event-call is used when a string is passed to ui-event
+ * Alternatively you can pass an object with keynames for events to ui-event
  * 
  * @example <input ui-event="blur" ui-event-call="myCallbackFunction">
+ * @example <input ui-event="{ focus : function(){alert('hi')}, blur : function(){alert('bye')} }">
  * 
- * @param ui-event {string} The event to bind to (can be multiple events separated by a space)
- * @param ui-event-call {function(event)} Callback function to execute upon event firing
+ * @param ui-event {string|object literal} The event to bind to as a string or a hash of events with their callbacks
+ * @param [ui-event-call] {function(event)} Callback function to execute upon event firing
+ * @param [ui-event-data] {object} Optional data to pass to the event callbacks
  */
 angular.module('ui.directives').directive('uiEvent', [function() {
 	return function(scope, elm, attrs) {
-		elm.bind(attrs.uiEvent, scope.$eval(attrs.uiEventCall));
+		var events = scope.$eval(attrs.uiEvent),
+			data = {};
+		if (attrs.uiEventData) {
+			data = scope.$eval(attrs.uiEventData);
+		}
+		if (angular.isString(event)) {
+			elm.bind(events, data, scope.$eval(attrs.uiEventCall));
+		} else {
+			elm.bind(events, data);
+		}
 	};
 }]);
 
