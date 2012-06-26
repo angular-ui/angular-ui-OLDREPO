01c4c26d487debe1cf048488df8ea844eafddc9d
Dean Sofer <dean@jobvite.com>
2012-06-26 11:46:02 -0700

    reBuilt scrollfix and select2 updates

--- modules/directives/scrollfix/src/scrollfix.js
+++ modules/directives/scrollfix/src/scrollfix.js
@@ -1,23 +1,22 @@
 
 /**
  * Adds a 'fixed' class to the element when the page scrolls past it's position.
- * @param [offset] {int} optional Y-offset to override the detected offset
+ * @param [offset] {int} optional Y-offset to override the detected offset.
+ *   Takes 300 (absolute) or -300 or +300 (relative to detected)
  */
 angular.module('ui.directives').directive('uiScrollfix', [function() {
   return function(scope, elm, attrs) {
-    var top = elm.offset().top,
-    original = '';
+    var top = elm.offset().top;
     if (!attrs.uiScrollfix) {
       attrs.uiScrollfix = top;
     } else {
-      original = attrs.uiScrollfix;
       if (attrs.uiScrollfix.indexOf('-') === 0) {
         attrs.uiScrollfix = top - attrs.uiScrollfix.substr(1);
       } else if (attrs.uiScrollfix.indexOf('+') === 0) {
         attrs.uiScrollfix = top + parseInt(attrs.uiScrollfix.substr(1));
       }
     }
-    $(window).unbind('scroll.ui-scrollfix'+original).bind('scroll.ui-scrollfix'+original, function(){
+    $(window).bind('scroll.ui-scrollfix', function(){
       if (!elm.hasClass('fixed') && window.pageYOffset > attrs.uiScrollfix) {
         elm.addClass('fixed');
       } else if (elm.hasClass('fixed') && window.pageYOffset < attrs.uiScrollfix) {
--- modules/directives/select2/src/select2.js
+++ modules/directives/select2/src/select2.js
@@ -18,79 +18,80 @@ angular.module('ui.directives').directive('uiSelect2', ['ui.config', '$http', fu
 	if (uiConfig.select2) {
 		angular.extend(options, uiConfig.select2);
 	}
-	return function(scope, elm, attrs) {
-		var init = true, // Only query the selected value's data when the plugin loads
-			opts, // instance-specific options
-			prevVal = '',
-			loaded = false,
-			ctrl = elm.data("$ngModelController");
+	return {
+		require: '?ngModel',
+		link: function(scope, elm, attrs, controller) {
+			var init = true, // Only query the selected value's data when the plugin loads
+				opts, // instance-specific options
+				prevVal = '',
+				loaded = false;
 
+			opts = angular.extend({}, options, scope.$eval(attrs.uiSelect2));
+			if (!elm.is('select') && attrs.multiple !== undefined) {
+				opts.multiple = true;
+				// Set the view and model value and update the angular template manually for the ajax/multiple select2.
+			     elm.bind("change", function(){
+			         controller.$setViewValue(elm.val());
+			         scope.$apply();
+			     });
+			}
 
-		opts = angular.extend({}, options, scope.$eval(attrs.uiSelect2));
-		if (attrs.multiple !== undefined) {
-			opts.multiple = true;
-			// Set the view and model value and update the angular template manually for the ajax/multiple select2.
-		     elm.bind("change", function(){
-		         ctrl.$setViewValue(elm.val());
-		         scope.$apply();
-		     });
-		}
-
-		function initialize(newVal) {
-			setTimeout(function(){
-				if (newVal !== undefined) {
-					if (opts.ajax) {
-						if (newVal && !$.isEmptyObject(newVal)) {
-							if (init && opts.initial) {
-								var url = opts.initial(opts.ajax.url, newVal, opts.multiple);
-							    $http({ method: 'GET', url: url }).success(function(data, status, headers, config){
-									data = opts.ajax.results(data);
-									elm.select2('val', data.results || '');
-								});
-								init = false;
+			function initialize(newVal) {
+				setTimeout(function(){
+					if (newVal !== undefined) {
+						if (opts.ajax) {
+							if (newVal && !$.isEmptyObject(newVal)) {
+								if (init && opts.initial) {
+									var url = opts.initial(opts.ajax.url, newVal, opts.multiple);
+								    $http({ method: 'GET', url: url }).success(function(data, status, headers, config){
+										data = opts.ajax.results(data);
+										elm.select2('val', data.results || '');
+									});
+									init = false;
+								}
+							} else {
+							    elm.select2('val', '');
 							}
 						} else {
-						    elm.select2('val', '');
+							elm.select2('val', newVal);
 						}
-					} else {
-						elm.select2('val', newVal);
+					}
+				},0);
+			}
+
+			// Initialize the plugin late so that the injected DOM does not disrupt the template compiler
+			// ToDo: $timeout service
+			setTimeout(function(){
+				elm.select2(opts);
+				loaded = true;
+				// If a watch was fired before initialized, set the init value
+				initialize(prevVal);
+			},0);
+
+			// Watch the model for programmatic changes
+			scope.$watch(attrs.ngModel, function(newVal, oldVal, scope) {
+				if (newVal === prevVal) {
+					return;
+				}
+				if (loaded) {
+					initialize(newVal);
+					if (!newVal) {
+					    // Push the model change to the view(only the null value in this case)
+					    elm.select2('val', '');
 					}
 				}
-			},0);
-		}
-
-		// Initialize the plugin late so that the injected DOM does not disrupt the template compiler
-		// ToDo: $timeout service
-		setTimeout(function(){
-			elm.select2(opts);
-			loaded = true;
-			// If a watch was fired before initialized, set the init value
-			initialize(prevVal);
-		},0);
-
-		// Watch the model for programmatic changes
-		scope.$watch(attrs.ngModel, function(newVal, oldVal, scope) {
-			if (newVal === prevVal) {
-				return;
-			}
-			if (loaded) {
-				initialize(newVal);
-				if (attrs.multiple !== undefined && !newVal) {
-				    // Push the model change to the view(only the null value in this case)
-				    elm.select2('val', '');
-				}
-			}
-			prevVal = newVal;
-		});
-		// If you want you can watch the options dataset for changes
-		if (angular.isString(opts.watch)) {
-			scope.$watch(opts.watch, function(newVal, oldVal, scope){
-				if (loaded && prevVal) {
-					setTimeout(function(){
-						elm.select2('val', prevVal);
-					},0);
-				}
+				prevVal = newVal;
 			});
+			// If you want you can watch the options dataset for changes
+			if (angular.isString(opts.watch)) {
+				scope.$watch(opts.watch, function(newVal, oldVal, scope){
+					if (loaded && prevVal) {
+						setTimeout(function(){
+							elm.select2('val', prevVal);
+						},0);
+					}
+				});
+			}
 		}
 	};
 }]);
