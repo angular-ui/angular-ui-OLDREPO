9cb02fece4246c33333ec085b164520784cfe711
Andriy Borodiychuk <noty@noty.net>
2012-07-07 11:21:57 +0200

    Refactored keypress dorective with adding keys-combinations and alternative declaration syntax. Backward compatible

--- modules/directives/keypress/src/keypress.js
+++ modules/directives/keypress/src/keypress.js
@@ -1,20 +1,77 @@
 
 /**
- * Bind an event to a particular keypress
- * @param hash {object} keyCode The number keycode to watch (ex: 13 is the return key). The callback function to fire upon keypress. Takes an 'event' param
- * @example <input ui-keypress="{ 13 : someCallback }">
+ * Bind one or more handlers to particular keys or their combination
+ * @param hash {mixed} keyBindings Can be an object or string where keybinding expression of keys or keys combinations and AngularJS Exspressions are set. Object syntax: "{ keys1: expression1 [, keys2: expression2 [ , ... ]]}".  String syntax: ""expression1 on keys1 [ and expression2 on keys2 [ and ... ]]"". Expression is an AngularJS Expression, and key(s) are dash-separated combinations of keys and modifiers (one or many, if any. Order does not matter). Supported modifiers are 'ctrl', 'shift', 'alt' and key can be used either via its keyCode (13 for Return) or name. Named keys are 'backspace', 'tab', 'enter', 'esc', 'space', 'pageup', 'pagedown', 'end', 'home', 'left', 'up', 'right', 'down', 'insert', 'delete'.
+ * @example <input ui-keypress="{enter:'x = 1', 'ctrl-shift-space':'foo()', 'shift-13':'bar()'}" /> <input ui-keypress="foo = 2 on ctrl-13 and bar('hello') on shift-esc" />
  **/
 angular.module('ui.directives').directive('uiKeypress', [function(){
-	return {
-		link: function(scope, elm, attrs) {
-			var params = scope.$eval(attrs.uiKeypress);
-			elm.bind('keypress', function(event){
-				if (params[event.keyCode]){
-					scope.$apply(function(){
-						params[event.keyCode](event);
-					});
-				}
-			});
-		}
-	};
+  return {
+    link: function(scope, elm, attrs) {
+      var keysByCode = {
+        8:  'backspace',
+        9:  'tab',
+        13: 'enter',
+        27: 'esc',
+        32: 'space',
+        33: 'pageup',
+        34: 'pagedown',
+        35: 'end',
+        36: 'home',
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        45: 'insert',
+        46: 'delete'
+      };
+
+      elm.bind('keydown', function(event) {
+        // By default no code to execute
+        var evalCode = '';
+        try {
+          var params = scope.$eval(attrs.uiKeypress);
+          var paramsParsed = true;
+        } catch (error) {
+          var params = attrs.uiKeypress.split(/\s+and\s+/i);
+          var paramsParsed = false;
+        }
+
+        // First of all we split binding of multiple keys
+        angular.forEach(params, function(v, k) {
+          if(paramsParsed) {
+            // An object passed
+            var expression = v;
+            var keys = k;
+          } else {
+            // A string passed
+            v = v.split(/\s+on\s+/i);
+            var expression = v[0];
+            var keys = v[1];
+          }
+
+          keys = keys.split('-');
+
+          var mainKeyPressed = keys.indexOf( keysByCode[event.keyCode] ) > -1 || keys.indexOf( event.keyCode.toString() ) > -1
+
+          var altPressed   = event.metaKey || event.altKey;
+          var ctrlPressed  = event.ctrlKey;
+          var shiftPressed = event.shiftKey;
+
+          var altRequired   =  keys.indexOf('alt')   > -1;
+          var ctrlRequired  =  keys.indexOf('ctrl')  > -1;
+          var shiftRequired =  keys.indexOf('shift') > -1;
+
+          if( mainKeyPressed &&
+              ( altRequired   == altPressed   ) &&
+              ( ctrlRequired  == ctrlPressed  ) &&
+              ( shiftRequired == shiftPressed )
+            ) {
+            evalCode = expression;
+          }
+        });
+        // Run the function
+        scope.$eval(evalCode);
+      });
+    }
+  };
 }]);
