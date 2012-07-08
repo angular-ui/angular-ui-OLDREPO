72637ac485fc80d4fb03d62f569d3b75089a6084
Andriy Borodiychuk <noty@noty.net>
2012-07-08 11:21:22 +0200

    Fixed indentation

--- modules/directives/keypress/src/keypress.js
+++ modules/directives/keypress/src/keypress.js
@@ -1,15 +1,15 @@
 
 /**
  * Bind one or more handlers to particular keys or their combination
- * @param hash {mixed} keyBindings Can be an object or string where keybinding expression of keys or keys combinations and AngularJS Exspressions are set. Object syntax: "{ keys1: expression1 [, keys2: expression2 [ , ... ]]}".	String syntax: ""expression1 on keys1 [ and expression2 on keys2 [ and ... ]]"". Expression is an AngularJS Expression, and key(s) are dash-separated combinations of keys and modifiers (one or many, if any. Order does not matter). Supported modifiers are 'ctrl', 'shift', 'alt' and key can be used either via its keyCode (13 for Return) or name. Named keys are 'backspace', 'tab', 'enter', 'esc', 'space', 'pageup', 'pagedown', 'end', 'home', 'left', 'up', 'right', 'down', 'insert', 'delete'.
+ * @param hash {mixed} keyBindings Can be an object or string where keybinding expression of keys or keys combinations and AngularJS Exspressions are set. Object syntax: "{ keys1: expression1 [, keys2: expression2 [ , ... ]]}". String syntax: ""expression1 on keys1 [ and expression2 on keys2 [ and ... ]]"". Expression is an AngularJS Expression, and key(s) are dash-separated combinations of keys and modifiers (one or many, if any. Order does not matter). Supported modifiers are 'ctrl', 'shift', 'alt' and key can be used either via its keyCode (13 for Return) or name. Named keys are 'backspace', 'tab', 'enter', 'esc', 'space', 'pageup', 'pagedown', 'end', 'home', 'left', 'up', 'right', 'down', 'insert', 'delete'.
  * @example <input ui-keypress="{enter:'x = 1', 'ctrl-shift-space':'foo()', 'shift-13':'bar()'}" /> <input ui-keypress="foo = 2 on ctrl-13 and bar('hello') on shift-esc" />
  **/
 angular.module('ui.directives').directive('uiKeypress', [function(){
 	return {
 		link: function(scope, elm, attrs) {
 			var keysByCode = {
-				8:	'backspace',
-				9:	'tab',
+				8:  'backspace',
+				9:  'tab',
 				13: 'enter',
 				27: 'esc',
 				32: 'space',
@@ -52,17 +52,17 @@ angular.module('ui.directives').directive('uiKeypress', [function(){
 
 					var mainKeyPressed = keys.indexOf( keysByCode[event.keyCode] ) > -1 || keys.indexOf( event.keyCode.toString() ) > -1
 
-					var altPressed	 = event.metaKey || event.altKey;
-					var ctrlPressed	= event.ctrlKey;
+					var altPressed   = event.metaKey || event.altKey;
+					var ctrlPressed  = event.ctrlKey;
 					var shiftPressed = event.shiftKey;
 
-					var altRequired	 =	keys.indexOf('alt')	 > -1;
-					var ctrlRequired	=	keys.indexOf('ctrl')	> -1;
-					var shiftRequired =	keys.indexOf('shift') > -1;
+					var altRequired   = keys.indexOf('alt')   > -1;
+					var ctrlRequired  = keys.indexOf('ctrl')  > -1;
+					var shiftRequired = keys.indexOf('shift') > -1;
 
 					if( mainKeyPressed &&
-							( altRequired	 == altPressed	 ) &&
-							( ctrlRequired	== ctrlPressed	) &&
+							( altRequired   == altPressed   ) &&
+							( ctrlRequired  == ctrlPressed  ) &&
 							( shiftRequired == shiftPressed )
 						) {
 						// Run the function
