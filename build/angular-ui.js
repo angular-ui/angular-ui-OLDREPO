384b14edb79e6a55cb90b5abb1de3fc7f6e33b8f
Pete Bacon Darwin <pete@bacondarwin.com>
2012-07-11 14:08:21 +0100

    Removed console.log and added some comments

--- build/angular-ui.js
+++ build/angular-ui.js
@@ -20,15 +20,19 @@ angular.module('ui.directives').directive('uiCodemirror', ['ui.config', function
             var defaults = {
                 onChange: function (ed) {
                     newValue = ed.getValue();
+                    // We only update the model if the value has changed - this helps get around a little problem where $render triggers a change despite already being inside a $apply loop.
                     if ( newValue !== ngModel.$viewValue ) {
                         ngModel.$setViewValue(ed.getValue());
                         scope.$apply();
                     }
                 }
             };
+            // Merge together the options from the uiConfig and the attribute itself with the onChange event above.
             var options = angular.extend(defaults, scope.$eval(attrs.uiCodemirror), uiConfig.codemirror);
-            console.log(options);
             var codemirror = CodeMirror.fromTextArea(elm[0], options);
+
+            // Override the ngModelController $render method, which is what gets called when the model is updated.
+            // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
             ngModel.$render = function() {
                 codemirror.setValue(ngModel.$viewValue);
             };
