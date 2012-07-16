091e96c56b16f98b3f3a455a83cad3fbc17a19b3
Dean Sofer <proloser@hotmail.com>
2012-07-16 16:38:51 -0700

    Removed the link: { post: function() } because I thought it was for something else
    Based on the documentation, I thought using a post-linking function is a safe bet
    loading plugins asyncronously, but it turns out linking functions are post by default.
    
    Reverting this change fixes the conflict with TinyMCE

--- modules/directives/scrollfix/src/scrollfix.js
+++ modules/directives/scrollfix/src/scrollfix.js
@@ -6,26 +6,24 @@
  */
 angular.module('ui.directives').directive('uiScrollfix', [function() {
 	return {
-		link: {
-			post: function(scope, elm, attrs) {
-				var top = elm.offset().top;
-				if (!attrs.uiScrollfix) {
-					attrs.uiScrollfix = top;
-				} else {
-					if (attrs.uiScrollfix.indexOf('-') === 0) {
-						attrs.uiScrollfix = top - attrs.uiScrollfix.substr(1);
-					} else if (attrs.uiScrollfix.indexOf('+') === 0) {
-						attrs.uiScrollfix = top + parseInt(attrs.uiScrollfix.substr(1));
-					}
+		link: function(scope, elm, attrs) {
+			var top = elm.offset().top;
+			if (!attrs.uiScrollfix) {
+				attrs.uiScrollfix = top;
+			} else {
+				if (attrs.uiScrollfix.indexOf('-') === 0) {
+					attrs.uiScrollfix = top - attrs.uiScrollfix.substr(1);
+				} else if (attrs.uiScrollfix.indexOf('+') === 0) {
+					attrs.uiScrollfix = top + parseInt(attrs.uiScrollfix.substr(1));
 				}
-				$(window).bind('scroll.ui-scrollfix', function(){
-					if (!elm.hasClass('ui-scrollfix') && window.pageYOffset > attrs.uiScrollfix) {
-						elm.addClass('ui-scrollfix');
-					} else if (elm.hasClass('ui-scrollfix') && window.pageYOffset < attrs.uiScrollfix) {
-						elm.removeClass('ui-scrollfix');
-					}
-				});
 			}
+			$(window).bind('scroll.ui-scrollfix', function(){
+				if (!elm.hasClass('ui-scrollfix') && window.pageYOffset > attrs.uiScrollfix) {
+					elm.addClass('ui-scrollfix');
+				} else if (elm.hasClass('ui-scrollfix') && window.pageYOffset < attrs.uiScrollfix) {
+					elm.removeClass('ui-scrollfix');
+				}
+			});
 		}
 	};
 }]);
--- modules/directives/tinymce/src/tinymce.js
+++ modules/directives/tinymce/src/tinymce.js
@@ -6,46 +6,46 @@ angular.module('ui.directives').directive('uiTinymce', ['ui.config', function(ui
 	uiConfig.tinymce = uiConfig.tinymce || {};
 	return {
 		require: 'ngModel',
-		link: {
-			post: function(scope, elm, attrs, ngModel) {
-				var expression,
-				  options = {
-					// Update model on button click
-					onchange_callback: function(inst) {
-						if (inst.isDirty()) {
-							inst.save();
-							ngModel.$setViewValue(elm.val());
-                            scope.$apply();
-						}
-					},
-					// Update model on keypress
-					handle_event_callback: function(e) {
-						if (this.isDirty()) {
-							this.save();
-							ngModel.$setViewValue(elm.val());
-                            scope.$apply();
-						}
-						return true; // Continue handling
-					},
-					// Update model when calling setContent (such as from the source editor popup)
-					setup : function(ed) {
-						ed.onSetContent.add(function(ed, o) {
-							if (ed.isDirty()) {
-								ed.save();
-								ngModel.$setViewValue(elm.val());
-								scope.$apply();
-							}
-						});
+		link: function(scope, elm, attrs, ngModel) {
+			var expression,
+			  options = {
+				// Update model on button click
+				onchange_callback: function(inst) {
+					if (inst.isDirty()) {
+						inst.save();
+						ngModel.$setViewValue(elm.val());
+                           scope.$apply();
 					}
-				};
-				if (attrs.uiTinymce) {
-					expression = scope.$eval(attrs.uiTinymce);
-				} else {
-					expression = {};
+				},
+				// Update model on keypress
+				handle_event_callback: function(e) {
+					if (this.isDirty()) {
+						this.save();
+						ngModel.$setViewValue(elm.val());
+                           scope.$apply();
+					}
+					return true; // Continue handling
+				},
+				// Update model when calling setContent (such as from the source editor popup)
+				setup : function(ed) {
+					ed.onSetContent.add(function(ed, o) {
+						if (ed.isDirty()) {
+							ed.save();
+							ngModel.$setViewValue(elm.val());
+							scope.$apply();
+						}
+					});
 				}
-				angular.extend(options, uiConfig.tinymce, expression);
-				elm.tinymce(options);
+			};
+			if (attrs.uiTinymce) {
+				expression = scope.$eval(attrs.uiTinymce);
+			} else {
+				expression = {};
 			}
+			angular.extend(options, uiConfig.tinymce, expression);
+			setTimeout(function(){
+				elm.tinymce(options);
+			});
 		}
 	};
 }]);
