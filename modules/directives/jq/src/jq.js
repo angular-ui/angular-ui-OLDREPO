d70e99b6971803ee1477cceda068d45eaef2f6b2
Doh Jone <doh.jone@gmail.com>
2012-07-16 14:56:19 -0700

    Changes to the passthrough plugin
    Merging mine and ProLoser's changes

--- modules/directives/jq/src/jq.js
+++ modules/directives/jq/src/jq.js
@@ -14,28 +14,26 @@
  * @example <input ui-jq="datepicker" ui-options="{showOn:'click'},secondParameter,thirdParameter">
  */
 angular.module('ui.directives').directive('uiJq', ['ui.config', function(uiConfig) {
-	var options = {};
-	return {
-		link: {
-			post: function(scope, elm, attrs) {
-				var evalOptions;
-				if (uiConfig['jq'] && uiConfig['jq'][attrs.uiJq]) {
-					if (angular.isObject(options) && angular.isObject(uiConfig['jq'][attrs.uiJq])) {
-						angular.extend(options, uiConfig['jq'][attrs.uiJq]);
-					} else {
-						options = uiConfig['jq'][attrs.uiJq];
-					}
-				}
-				if (attrs.uiOptions) {
-					evalOptions = scope.$eval('['+attrs.uiOptions+']');
-					if (angular.isObject(options) && angular.isObject(evalOptions[0])) {
-						angular.extend(options, evalOptions[0]);
-					} else {
-						options = evalOptions[0];
-					}
-				}
-				elm[attrs.uiJq](options);
-			}
-		}
-	};
+    var options = {};
+    return {
+        link: {
+            post: function(scope, elm, attrs) {
+                var evalOptions;
+                if (uiConfig['jq'] && uiConfig['jq'][attrs.uiJq]) {
+                    if (angular.isObject(options) && angular.isObject(uiConfig['jq'][attrs.uiJq])) {
+                        angular.extend(options, uiConfig['jq'][attrs.uiJq]);
+                    } else {
+                        options = uiConfig['jq'][attrs.uiJq];
+                    }
+                }
+                if (attrs.uiOptions) {
+                    evalOptions = scope.$eval('['+attrs.uiOptions+']');
+                    if (angular.isObject(options) && angular.isObject(evalOptions[0])) {
+                        evalOptions = angular.extend(options, evalOptions[0]);
+                    }
+                }
+                elm[attrs.uiJq](evalOptions);
+            }
+        }
+    };
 }]);
