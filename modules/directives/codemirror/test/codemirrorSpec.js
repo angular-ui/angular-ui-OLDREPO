40e76d3407c28adb8d9cd364880839342cd9a3f2
Christopher Hiller <chiller@decipherinc.com>
2012-07-18 20:25:55 -0700

    removing cruft from #62 fix

--- modules/directives/codemirror/test/codemirrorSpec.js
+++ modules/directives/codemirror/test/codemirrorSpec.js
@@ -77,12 +77,6 @@ describe('uiCodemirror', function () {
     describe('when the model is falsy', function () {
         it('should update the IDE with an empty string', function () {
             inject(function ($compile) {
-                var codemirror,
-                    fromTextArea = CodeMirror.fromTextArea;
-                spyOn(CodeMirror, 'fromTextArea').andCallFake(function () {
-                    codemirror = fromTextArea.apply(this, arguments);
-                    return codemirror;
-                });
                 var element = $compile('<textarea ui-codemirror ng-model="foo"></textarea>')(scope);
                 scope.$apply();
                 expect(scope.foo).toBe(undefined);
