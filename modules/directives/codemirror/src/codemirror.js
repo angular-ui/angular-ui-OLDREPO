853f9bc448f171ded1a112308f9e6528d4ccb091
Christopher Hiller <chiller@decipherinc.com>
2012-07-18 19:59:59 -0700

    fixes issue #62: CodeMirror expects a string

--- modules/directives/codemirror/src/codemirror.js
+++ modules/directives/codemirror/src/codemirror.js
@@ -56,7 +56,17 @@ angular.module('ui.directives').directive('uiCodemirror', ['ui.config', '$parse'
             // Override the ngModelController $render method, which is what gets called when the model is updated.
             // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
             ngModel.$render = function() {
-                codemirror.setValue(ngModel.$viewValue);
+                if(angular.isString(ngModel.$viewValue)) {
+                    codemirror.setValue(ngModel.$viewValue);
+                } else {
+                    // momentarily stub out the change handler.  we want to keep the model
+                    // value, but CodeMirror wants a string, so we pass it an empty string
+                    // and keep the model the same.
+                    var changeHandler = codemirror.getOption('onChange');
+                    codemirror.setOption('onChange', angular.noop);
+                    codemirror.setValue('');
+                    codemirror.setOption('onChange', changeHandler);
+                }
             };
        }
     };
--- modules/directives/codemirror/test/codemirrorSpec.js
+++ modules/directives/codemirror/test/codemirrorSpec.js
@@ -74,4 +74,20 @@ describe('uiCodemirror', function () {
             });
         });
     });
+    describe('when the model is falsy', function () {
+        it('should update the IDE with an empty string', function () {
+            inject(function ($compile) {
+                var codemirror,
+                    fromTextArea = CodeMirror.fromTextArea;
+                spyOn(CodeMirror, 'fromTextArea').andCallFake(function () {
+                    codemirror = fromTextArea.apply(this, arguments);
+                    return codemirror;
+                });
+                var element = $compile('<textarea ui-codemirror ng-model="foo"></textarea>')(scope);
+                scope.$apply();
+                expect(scope.foo).toBe(undefined);
+                expect(element.siblings().text().trim()).toBe('');
+            });
+        });
+    });
 });
