aacd6dfa87b6b1f191bca22aa803a82f48ac8faa
Andy Joslin <andytjoslin@gmail.com>
2012-07-20 12:58:35 -0400

    added tests for infowindow/map, added directives for the rest of the overlays

--- modules/directives/map/src/map.js
+++ modules/directives/map/src/map.js
@@ -1,4 +1,5 @@
 (function() {
+  var app = angular.module('ui.directives');
 
   //Setup map events from a google map object to trigger on a given element too,
   //then we just use ui-event to catch events from an element
@@ -14,15 +15,15 @@
         if (!scope.$$phase) scope.$apply();
       });
     });
-  };
+  }
 
-  angular.module('ui.directives')
-  .directive('uiMap', ['ui.config', '$parse', function(uiConfig, $parse) {
+  app.directive('uiMap', 
+  ['ui.config', '$parse', function(uiConfig, $parse) {
 
     var mapEvents = 'bounds_changed center_changed click dblclick drag dragend '+
-  'dragstart heading_changed idle maptypeid_changed mousemove mouseout '+
-  'mouseover projection_changed resize rightclick tilesloaded tilt_changed '+
-  'zoom_changed';
+    'dragstart heading_changed idle maptypeid_changed mousemove mouseout '+
+    'mouseover projection_changed resize rightclick tilesloaded tilt_changed '+
+    'zoom_changed';
     var options = uiConfig.map || {};
     
     return {
@@ -32,25 +33,24 @@
         var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
         var map = new google.maps.Map(elm[0], opts);
         var model = $parse(attrs.uiMap);
-    
+
         //Set scope variable for the map
         model.assign(scope, map); 
-    
+
         bindMapEvents(scope, mapEvents, map, elm);
       }
     };
-  }])
+  }]);
 
-  .directive('uiMapInfoWindow', 
+  app.directive('uiMapInfoWindow', 
   ['ui.config', '$parse', '$compile', function(uiConfig, $parse, $compile) {
-    
+
     var infoWindowEvents = 'closeclick content_change domready '+
     'position_changed zindex_changed';
     var options = uiConfig.mapInfoWindow || {};
-    
+
     return {
       link: function(scope, elm, attrs) {
-        var _open;
         var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
         opts.content = elm[0];
         var model = $parse(attrs.uiMapInfoWindow);
@@ -62,47 +62,57 @@
         }
 
         bindMapEvents(scope, infoWindowEvents, infoWindow, elm);
-        
-        //The info window element doesn't need to stay in the DOM, 
-        //so we take it out. but we can't just remove an element from the dom
-        //or angular will mess up. so we replace it with an empty div
-        elm.replaceWith('<div></div>');
 
-        //Each time info window opens, re-$compile the contents
-        _open = infoWindow.open;
-        infoWindow.open = function(arg1,arg2,arg3,arg4,arg5,arg6) {
+        elm.replaceWith('<div></div>');
+        var _open = infoWindow.open;
+        infoWindow.open = function(map,marker) {
           $compile(elm.contents())(scope);
-          _open.call(infoWindow, arg1,arg2,arg3,arg4,arg5,arg6); 
+          _open.call(infoWindow, map, marker); 
         }
       }
     };
-  }])
+  }]);
 
-  //ui-marker directive
-  //This finds a marker on the scope matching the ui-marker given.
-  //eg <ui-marker="myMarker"> will look for scope.myMarker and hook up to it,
-  //allowing events to be dispatched to myMarker
-  .directive('uiMarker', ['ui.config', '$parse', function(uiConfig, $parse) {
-    
-    var markerEvents = 'animation_changed click clickable_changed cursor_changed '+
+  /* 
+  * Map overlay directives all work the same. Take map marker for example
+  * <ui-map-marker="myMarker"> will $watch 'myMarker' and each time it changes,
+  * it will hook up myMarker's events to the directive dom element.  Then
+  * ui-event will be able to catch all of myMarker's events. Super simple.
+  */
+  function mapOverlayDirective(directiveName, events) {
+    app.directive(directiveName, [function() {
+      return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+          scope.$watch(attrs[directiveName], function(newObject) {
+            bindMapEvents(scope, events, newObject, elm);
+          });
+        }
+      };
+    }]);
+  };
+
+  mapOverlayDirective('uiMapMarker', 
+    'animation_changed click clickable_changed cursor_changed '+
     'dblclick drag dragend draggable_changed dragstart flat_changed icon_changed '+
     'mousedown mouseout mouseover mouseup position_changed rightclick '+
-    'shadow_changed shape_changed title_changed visible_changed zindex_changed';
-    
-    return {
-      restrict: 'A',
-      link: function(scope, elm, attrs) {
-        
-        function setupMarker(marker) {
-          bindMapEvents(scope, markerEvents, marker, elm);
-        }
-        
-        //Once someone sets scope[attrs.uiMarker], hook it up to this element
-        scope.$watch(attrs.uiMarker, function(newMarker) {
-          if (newMarker) setupMarker(newMarker);
-        });
-      } 
-    }
-  }]);
+    'shadow_changed shape_changed title_changed visible_changed zindex_changed');
+  
+  mapOverlayDirective('uiMapPolyline', 
+    'click dblclick mousedown mousemove mouseout mouseover mouseup rightclick');
+  
+  mapOverlayDirective('uiMapPolygon', 
+    'click dblclick mousedown mousemove mouseout mouseover mouseup rightclick');
+  
+  mapOverlayDirective('uiMapRectangle', 
+    'bounds_changed click dblclick mousedown mousemove mouseout mouseover '+
+    'mouseup rightclick');
+  
+  mapOverlayDirective('uiMapCircle', 
+    'center_changed click dblclick mousedown mousemove '+
+    'mouseout mouseover mouseup radius_changed rightclick');
+  
+  mapOverlayDirective('uiMapGroundOverlay', 
+    'click dblclick');
 
 })();
