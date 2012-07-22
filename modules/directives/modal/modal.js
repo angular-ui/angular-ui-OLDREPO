1b0217748cfa36e555b8f5cf0be34b63ef25d593
Andy Joslin <andytjoslin@gmail.com>
2012-07-22 16:36:24 -0400

    made ui-modal restrict EAC for more freedom of use

--- modules/directives/modal/modal.js
+++ modules/directives/modal/modal.js
@@ -1,22 +1,22 @@
-
 angular.module('ui.directives')
 .directive('uiModal', ['$timeout', function($timeout) {
   return {
+    restrict: 'EAC',
     require: 'ngModel',
     link: function(scope, elm, attrs, model) {
       //helper so you don't have to type class="modal hide"
-      elm.addClass('modal hide'); 
+      elm.addClass('modal hide');
       scope.$watch(attrs.ngModel, function(value) {
-          elm.modal(value && 'show' || 'hide');
+        elm.modal(value && 'show' || 'hide');
       });
       elm.on('show.ui', function() {
-		$timeout(function() {
-    		 model.$setViewValue(true);
-    	});
+        $timeout(function() {
+          model.$setViewValue(true);
+        });
       });
       elm.on('hide.ui', function() {
         $timeout(function() {
-        	model.$setViewValue(false);
+          model.$setViewValue(false);
         });
       });
     }
