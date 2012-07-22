b406c1aa587c794e758ac83e8a995bc427e32c55
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-07-22 22:02:45 +0200

    Unit tests and fixes for the unique filter

--- modules/filters/unique/unique.js
+++ modules/filters/unique/unique.js
@@ -7,23 +7,39 @@
  * @return {array}
  */
 angular.module('ui.filters').filter('unique', function() {
-	return function(items, key) {
-		if (key && angular.isArray(items)) {
-			var hashCheck = {},
-				newItems = [];
-			angular.forEach(items, function(item, key){
-				var value;
-				if (angular.isString(key)) {
-					value = item[key];
-				} else {
-					value = item;
-				}
-				if (hashCheck[value] === undefined) {
-					hashCheck[value] = true;
-					newItems.push(item);
-				}
-			});
-			items = newItems;
+
+	return function(items, filterOn) {
+
+    if (filterOn===false){
+      return items;
+    }
+
+		if ((filterOn || angular.isUndefined(filterOn))&& angular.isArray(items)) {
+			var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function(item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate){
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
 		}
 		return items;
 	};
