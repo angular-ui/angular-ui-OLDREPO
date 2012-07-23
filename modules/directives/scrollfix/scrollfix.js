abbba7641231dbd762bd91eedbba29f621eb530c
Christopher Hiller <chiller@decipherinc.com>
2012-07-23 14:01:07 -0700

    #91: using angular.element() and 'on' function instead of bind

--- modules/directives/scrollfix/scrollfix.js
+++ modules/directives/scrollfix/scrollfix.js
@@ -19,7 +19,7 @@ angular.module('ui.directives').directive('uiScrollfix', ['$window', function ($
                     attrs.uiScrollfix = top + attrs.uiScrollfix.substr(1);
                 }
             }
-            $($window).bind('scroll.ui-scrollfix', function () {
+            angular.element($window).on('scroll.ui-scrollfix', function () {
                 // if pageYOffset is defined use it, otherwise use other crap for IE
                 var offset;
                 if (angular.isDefined($window.pageYOffset)) {
