29a1a7d9813882f9dad7874dabcf47617d73feb4
Andy Joslin <andytjoslin@gmail.com>
2012-07-27 18:33:07 -0400

    edited uiEvent docs to reflect $event and $params usability

--- modules/directives/event/event.js
+++ modules/directives/event/event.js
@@ -2,8 +2,10 @@
 /**
  * General-purpose Event binding. Bind any event not natively supported by Angular
  * Pass an object with keynames for events to ui-event
- * 
+ * Allows $event object and $params object to be passed
+ *
  * @example <input ui-event="{ focus : 'counter++', blur : 'someCallback()' }">
+ * @example <input ui-event="{ myCustomEvent : 'myEventHandler($event, $params)'}">
  * 
  * @param ui-event {string|object literal} The event to bind to as a string or a hash of events with their callbacks
  */
