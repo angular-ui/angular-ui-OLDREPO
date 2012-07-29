df639bb6fd6b8da52f0d4c568bdaf7dc91a81d73
Christopher Hiller <chiller@decipherinc.com>
2012-07-29 00:06:07 -0700

    #92: adding hide/toggle tests, some tweaks

--- modules/directives/showhide/test/showhideSpec.js
+++ modules/directives/showhide/test/showhideSpec.js
@@ -1,4 +1,4 @@
-/*global beforeEach, afterEach, describe, it, inject, expect, module, spyOn, CodeMirror, angular, $*/
+/*global beforeEach, describe, it, inject, expect, module, spyOn*/
 
 (function () {
     'use strict';
@@ -6,21 +6,21 @@
     describe('uiShow', function () {
 
         var scope, $compile;
-        beforeEach(module('ui'));
+        beforeEach(module('ui.directives'));
         beforeEach(inject(function (_$rootScope_, _$compile_) {
             scope = _$rootScope_.$new();
             $compile = _$compile_;
         }));
 
-        describe('when the directive is linked', function () {
+        describe('linking the directive', function () {
             it('should call scope.$watch', function () {
                 spyOn(scope, '$watch');
-                $compile('<div ui-show="true"></div>')(scope);
+                $compile('<div ui-show="foo"></div>')(scope);
                 expect(scope.$watch).toHaveBeenCalled();
             });
         });
 
-        describe('when a watch fires', function () {
+        describe('executing the watcher', function () {
             it('should add the ui-show class if true', function () {
                 var element = $compile('<div ui-show="foo"></div>')(scope);
                 scope.foo = true;
@@ -32,6 +32,72 @@
                 scope.foo = false;
                 scope.$apply();
                 expect(element.hasClass('ui-show')).toBe(false);
+            });
+        });
+    });
+
+    describe('uiHide', function () {
+
+        var scope, $compile;
+        beforeEach(module('ui.directives'));
+        beforeEach(inject(function (_$rootScope_, _$compile_) {
+            scope = _$rootScope_.$new();
+            $compile = _$compile_;
+        }));
+
+        describe('when the directive is linked', function () {
+            it('should call scope.$watch', function () {
+                spyOn(scope, '$watch');
+                $compile('<div ui-hide="foo"></div>')(scope);
+                expect(scope.$watch).toHaveBeenCalled();
+            });
+        });
+
+        describe('executing the watcher', function () {
+            it('should add the ui-hide class if true', function () {
+                var element = $compile('<div ui-hide="foo"></div>')(scope);
+                scope.foo = true;
+                scope.$apply();
+                expect(element.hasClass('ui-hide')).toBe(true);
+            });
+            it('should remove the ui-hide class if false', function () {
+                var element = $compile('<div ui-hide="foo"></div>')(scope);
+                scope.foo = false;
+                scope.$apply();
+                expect(element.hasClass('ui-hide')).toBe(false);
+            });
+        });
+    });
+
+    describe('uiToggle', function () {
+
+        var scope, $compile;
+        beforeEach(module('ui.directives'));
+        beforeEach(inject(function (_$rootScope_, _$compile_) {
+            scope = _$rootScope_.$new();
+            $compile = _$compile_;
+        }));
+
+        describe('when the directive is linked', function () {
+            it('should call scope.$watch', function () {
+                spyOn(scope, '$watch');
+                $compile('<div ui-toggle="foo"></div>')(scope);
+                expect(scope.$watch).toHaveBeenCalled();
+            });
+        });
+
+        describe('executing the watcher', function () {
+            it('should remove the ui-hide class and add the ui-show class if true', function () {
+                var element = $compile('<div ui-toggle="foo"></div>')(scope);
+                scope.foo = true;
+                scope.$apply();
+                expect(element.hasClass('ui-show') && !element.hasClass('ui-hide')).toBe(true);
+            });
+            it('should remove the ui-hide class and add the ui-show class if false', function () {
+                var element = $compile('<div ui-toggle="foo"></div>')(scope);
+                scope.foo = false;
+                scope.$apply();
+                expect(!element.hasClass('ui-show') && element.hasClass('ui-hide')).toBe(true);
             });
         });
     });
