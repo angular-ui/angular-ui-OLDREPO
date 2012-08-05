c65d5f3bf4b864b3d02438f4d155e38dd64ddd56
dandoyon <dandoyon@yahoo.com>
2012-08-05 12:57:38 -0700

    Implemented variable and underscore inflectors. Updated inflectorSpec

--- modules/filters/inflector/inflector.js
+++ modules/filters/inflector/inflector.js
@@ -13,7 +13,7 @@ angular.module('ui.filters').filter('inflector', function () {
     var inflectors = {
         humanize: function(value) {
             return value
-            //replace all _ and spaces and first characters in a word with upper case character
+            // replace all _ and spaces and first characters in a word with upper case character
             .replace(/(?:_| |\b)(\w)/g, function(str, p1) { return p1.toUpperCase();})
             // insert a space between lower & upper
             .replace(/([a-z])([A-Z])/g, '$1 $2')
@@ -21,17 +21,38 @@ angular.module('ui.filters').filter('inflector', function () {
             .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3');
         },
         underscore: function(value) {
-            // TODO
-            return value;
+            return value
+            // replace all _ and spaces and first characters in a word with upper case character
+            // uppercase is used as a temporary thing so that regex is simple to do underscore replacement later
+            .replace(/(?:_| |\b)(\w)/g, function(str, p1) { return p1.toUpperCase();})
+            // insert an underscore between lower & upper
+            .replace(/([a-z])([A-Z])/g, '$1_$2')
+            // underscore before last upper in a sequence followed by lower
+            .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1_$2$3').toLowerCase();
         },
         variable: function(value) {
-            // TODO
-            return value;
+            // replace all _ and spaces and first characters in a word with upper case character
+            value = value.replace(/(?:_| |\b)(\w)/g, function(str, p1) { return p1.toUpperCase();});   
+            // lowercase first letter
+            return value.charAt(0).toLowerCase() + value.slice(1)
         }
     };
     
     return function (text, inflector) {
         inflector = inflector || 'humanize';
-        return inflectors[inflector](text);
+        // massage text first, since this pertains to all inflectors, in future this may not be case
+        // i'm pretty sure that this could be done in fewer replaces with a complex regex. At least this is readable
+        // not sure how performant it will be...
+        text = text
+            // replace multiple underscores with single
+            .replace(/_{2,}/g,'_')
+            // replace multiple spaces with single space
+            .replace(/ {2,}/g,' ')
+            // remove spaces before and after underscore 
+            .replace(/(_ | _)/g,'_')
+            // trim leading/trailing underscores/spaces, if any
+            .replace(/(^_|_$|^ | $)/,'');
+
+        return inflectors[inflector](text).replace(/_$/,''); // for complex case, need to look for trailing underscore
     };
 });
