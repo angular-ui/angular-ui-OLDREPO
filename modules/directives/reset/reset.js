bbde578bce8f062f58a929a24958001c678f67d3
Dean Sofer <proloser@hotmail.com>
2012-08-04 19:13:32 -0700

    Fixes #123

--- modules/directives/reset/reset.js
+++ modules/directives/reset/reset.js
@@ -3,18 +3,16 @@
  * Add a clear button to form inputs to reset their value
  */
 angular.module('ui.directives').directive('uiReset', ['$parse', function($parse) {
-	return function(scope, elm, attrs) {
-		if (!attrs.ngModel) {
-			throw Error('ui-reset depends on ng-model');
-		}
-        // This object is a 'parsed' version of the model
-        var ngModel = $parse(attrs.ngModel);
-
-		elm.wrap('<span class="ui-resetwrap" />').after('<a class="ui-reset" />').next().click(function(e){
-			e.preventDefault();
-			// This lets you SET the value of the 'parsed' model
-			ngModel.assign(scope, null);
-			scope.$apply();
-		});
-	};
+  return {
+    require:'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+  		elm.wrap('<span class="ui-resetwrap" />').after('<a class="ui-reset" />').next().click(function(e){
+  			e.preventDefault();
+  			scope.$apply(function(){
+    			// This lets you SET the value of the 'parsed' model
+    			ctrl.$setViewValue(null);
+  			});
+  		});
+  	}
+  };
 }]);
