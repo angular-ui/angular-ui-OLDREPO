fc57d3a6a9692182b89e4490f1478f2dff3a2bc4
dandoyon <dandoyon@yahoo.com>
2012-08-16 17:24:50 -0700

    Reimplemented oneway

--- modules/directives/currency/currency.js
+++ modules/directives/currency/currency.js
@@ -2,56 +2,70 @@
 /*
  Gives the ability to style currency based on its sign.
 */
-  angular.module('ui.directives').directive('uiCurrency', ['ui.config','currencyFilter' , function(uiConfig, currencyFilter) {
+  angular.module('ui.directives').directive('uiCurrency', ['ui.config','currencyFilter' , '$interpolate', function(uiConfig, currencyFilter, $interpolate) {
 	  var options = {
 	      pos: 'ui-currency-pos',
 	      neg: 'ui-currency-neg',
-	      zero: 'ui-currency-zero'
-	};
-	if (uiConfig.currency) {
-		angular.extend(options, uiConfig.currency);
-	}
-    return {
+	      zero: 'ui-currency-zero',
+        bignum: 0,
+        smallnum: 0
+  	};
+  	if (uiConfig.currency) {
+  		angular.extend(options, uiConfig.currency);
+  	}
+    return { 
       restrict: 'EAC',
-      require: 'ngModel',
+      require: '?ngModel',
       link: function(scope, element, attrs, controller) {
-        var opts, // instance-specific options
-          renderview, 
-          value;
-      
+        var opts, renderview, value, removeclasses, isnottotal; // instance-specific options
+
         opts = angular.extend({}, options, scope.$eval(attrs.uiCurrency));
         
+        isnottotal = angular.isUndefined(attrs.isTotal);
+
+        // since we're going to possibly add/remove, it's easier just to remove them all first
+        element.removeClass(opts.pos);
+        element.removeClass(opts.neg);
+        element.removeClass(opts.zero);
+        element.removeClass('ui-bignum');
+        element.removeClass('ui-smallnum');
+
         renderview = function(viewvalue) {
           var num;
+          viewvalue = $.trim(viewvalue);
+          if(viewvalue == '') {
+            element.text('');
+            return;
+          }
           num = viewvalue * 1;
+
           if (num > 0) {
             element.addClass(opts.pos);
-          } else {
-            element.removeClass(opts.pos);
-          }
-          if (num < 0) {
+            if(isnottotal && opts.bignum && num >= opts.bignum) {
+               element.addClass('ui-bignum');
+            }
+          } else if (num < 0) {
             element.addClass(opts.neg);
-          } else {
-            element.removeClass(opts.neg);
-          }
-          if (num === 0) {
+            if(isnottotal && opts.smallnum && num <= opts.smallnum) {
+               element.addClass('ui-smallnum');
+            }
+          } else if (num === 0) {
             element.addClass(opts.zero);
-          } else {
-            element.removeClass(opts.zero);
           }
-          if (viewvalue === '') {
-            element.text('');
-          } else {
-            element.text(currencyFilter(num, opts.symbol));
-          }
-          return true;
+          element.text(currencyFilter(num, opts.symbol));
         };
-        
-        controller.$render = function() {
-          value = controller.$viewValue;
-          element.val(value);
+        if (controller != null) {
+          controller.$render = function() {
+            value = controller.$viewValue;
+            element.val(value);
+            renderview(value);
+          };
+        } else {
+          var exp;
+          exp = $interpolate(element.text());
+          value = exp(scope);
           renderview(value);
-        };
+        }
         
       }
     };
--- modules/directives/currency/test/currencySpec.js
+++ modules/directives/currency/test/currencySpec.js
@@ -1,11 +1,11 @@
   describe('uiCurrency', function() {
     var scope;
-    scope = null,
-    uiConfig = {};
+    scope = null;
     beforeEach(module('ui'));
     beforeEach(inject(function($rootScope) {
       scope = $rootScope.$new();
     }));
+
     describe('use on a div element with two-way binding', function() {
      it('should have ui-currency-pos style non-zero positive model number ', function() {
         inject(function($compile) {
@@ -79,6 +79,86 @@
           expect(element.hasClass('pstyle')).toBeTruthy();
         });
       });
+
+      it('should have big class when model is greater than certain positive value', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency=\"{ bignum:'100' }\" ng-model='aNum'></div>")(scope);
+          scope.$apply(function() {
+            scope.aNum = 100;
+          });
+          expect(element.hasClass('ui-bignum')).toBeTruthy();
+        });
+      });
+
+      it('should not have big class when model is not greater than certain positive value', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency=\"{ bignum:'100' }\" ng-model='aNum'></div>")(scope);
+          scope.$apply(function() {
+            scope.aNum = 99;
+          });
+          expect(element.hasClass('ui-bignum')).toBeFalsy();
+        });
+      });
+
+      it('should have small class when model is lesser than certain negative value', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency=\"{ smallnum:'-100' }\" ng-model='aNum'></div>")(scope);
+          scope.$apply(function() {
+            scope.aNum = -100;
+          });
+          expect(element.hasClass('ui-smallnum')).toBeTruthy();
+        });
+      });
+
+      it('should not have small class when model is not lesser than certain negative value', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency=\"{ smallnum:'-100' }\" ng-model='aNum'></div>")(scope);
+          scope.$apply(function() {
+            scope.aNum = -99;
+          });
+          expect(element.hasClass('ui-smallnum')).toBeFalsy();
+        });
+      });
+
+      it('should not have big or small class when model model is zero', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency=\"{ smallnum:'-100', bignum:'0' }\" ng-model='aNum'></div>")(scope);
+          scope.$apply(function() {
+            scope.aNum = 0;
+          });
+          expect(element.hasClass('ui-bignum')).toBeFalsy();
+        });
+      });
+
+        it('should not have a small class when is-total attribute is set ', function() {
+          inject(function($compile) {
+            var element;
+            element = $compile("<div ui-currency=\"{ smallnum:'-100' }\" is-total ng-model='aNum'></div>")(scope);
+            scope.$apply(function() {
+              scope.aNum = -100;
+            });
+            expect(element.hasClass('ui-smallnum')).toBeFalsy();
+          });
+        });
+
+
+        it('should not have a big class when is-total attribute is set ', function() {
+          inject(function($compile) {
+            var element;
+            element = $compile("<div ui-currency=\"{ bignum:'100' }\" is-total ng-model='aNum'></div>")(scope);
+            scope.$apply(function() {
+              scope.aNum = 100;
+            });
+            expect(element.hasClass('ui-bignum')).toBeFalsy();
+          });
+        });
+
+
       // Presumption is if above works then no need to test other cases, given the coverage in previous describe
     });
     describe('use on a tag element', function() {
@@ -94,4 +174,65 @@
         });
       });
     });
+
+    describe('use on a div element with curlies', function() {
+      it('should have ui-currency-pos style non-zero positive number', function() {
+        inject(function($compile) {
+          var element;
+          scope.aNum = 0.5123;
+          element = $compile("<div ui-currency>{{aNum}}</div>")(scope);
+          expect(element.text()).toEqual('$0.51');
+          expect(element.hasClass('ui-currency-pos')).toBeTruthy();
+        });
+      });
+      it('should have ui-currency-neg style when negative number', function() {
+        inject(function($compile) {
+          var element;
+          scope.aNum = -123;
+          element = $compile("<div ui-currency>{{aNum}}</div>")(scope);
+          expect(element.text()).toEqual('($123.00)');
+          expect(element.hasClass('ui-currency-neg')).toBeTruthy();
+         });
+      });
+      it('should have ui-currency-zero style when zero', function() {
+        inject(function($compile) {
+          var element;
+          scope.aNum = 0;
+          element = $compile("<div ui-currency>{{aNum}}</div>")(scope);
+          expect(element.text()).toEqual('$0.00');
+          expect(element.hasClass('ui-currency-zero')).toBeTruthy();
+        });
+      });
+      it('should not have any ui-currency styles or a value at all when missing scope model value', function() {
+        inject(function($compile) {
+          var element;
+          element = $compile("<div ui-currency>{{aMissingNum}}</div>")(scope);
+          expect(element.hasClass('ui-currency-zero')).toBeFalsy();
+        });
+      });
+      it('should not have any ui-currency styles or a value at all when provided a non-numeric number', function() {
+        inject(function($compile) {
+          var element;
+          scope.aBadNum = 'bad';
+          element = $compile("<div ui-currency>{{aBadNum}}</div>")(scope);
+          expect(element.text()).toEqual('');
+          expect(element.hasClass('ui-currency-pos')).toBeFalsy();
+          expect(element.hasClass('ui-currency-neg')).toBeFalsy();
+          expect(element.hasClass('ui-currency-zero')).toBeFalsy();
+        });
+      });
+      it('should not have any ui-currency styles or a value at all when provided an empty string', function() {
+        inject(function($compile) {
+          var element;
+          scope.aBadNum = '      ';
+          element = $compile("<div ui-currency>{{aBadNum}}</div>")(scope);
+          expect(element.text()).toEqual('');
+          expect(element.hasClass('ui-currency-pos')).toBeFalsy();
+          expect(element.hasClass('ui-currency-neg')).toBeFalsy();
+          expect(element.hasClass('ui-currency-zero')).toBeFalsy();
+        });
+      });
+    });
+
   });
+
