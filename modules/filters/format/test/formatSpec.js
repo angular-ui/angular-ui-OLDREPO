cff71e091d5d49b68f7882fa8a47b27e0999c9e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-08-19 20:25:37 +0200

    Format filter spec fixes

--- modules/filters/format/test/formatSpec.js
+++ modules/filters/format/test/formatSpec.js
@@ -10,10 +10,10 @@ describe('format', function() {
     expect(formatFilter('First $0, then $0, finally $0', 'bob')).toEqual('First bob, then bob, finally bob');
   });
   it('should replace all instances of $n based on order of token array', function() {
-    expect(formatFilter('First $0, then $1, finally $2', ['bob','frank','dianne'])).toEqual('First is bob, then frank, finally dianne');
+    expect(formatFilter('First is $0, then $1, finally $2', ['bob','frank','dianne'])).toEqual('First is bob, then frank, finally dianne');
   });
   it('should replace all instances :tokens based on keys of token object', function() {
-    expect(formatFilter('First is :first, next is :second, finally there is :third', {first:'bob',second:'frank',third:'dianne'})).toEqual('First is bob, then frank, finally dianne');
+    expect(formatFilter('First is :first, next is :second, finally there is :third', {first:'bob',second:'frank',third:'dianne'})).toEqual('First is bob, next is frank, finally there is dianne');
   });
   it('should do nothing if tokens are undefined', function() {
     expect(formatFilter('Hello There')).toEqual('Hello There');
