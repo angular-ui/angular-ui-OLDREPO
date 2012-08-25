ff6d6216e022b79c397465e6cf2786698da14ce2
Christopher Hiller <chiller@decipherinc.com>
2012-08-25 10:11:10 -0700

    bug 161: delinting

--- modules/directives/date/test/dateSpec.js
+++ modules/directives/date/test/dateSpec.js
@@ -1,5 +1,6 @@
-
+/*global describe, beforeEach, it, inject, expect, module*/
 describe('uiDate', function() {
+  'use strict';
   var selectDate;
   selectDate = function(element, date) {
     element.datepicker('setDate', date);
