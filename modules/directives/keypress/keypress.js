6350d77c30339d15f2ed609980424c7ac5fa236f
Dean Sofer <proloser@hotmail.com>
2012-08-26 01:16:22 -0700

    Added variety tests and converted helper to a factory

--- modules/directives/keypress/keypress.js
+++ modules/directives/keypress/keypress.js
@@ -1,5 +1,5 @@
-function keypress(mode, scope, elm, attrs, $parse){
-  var params, paramsParsed, expression, keys, combinations = [], keysByCode = {
+angular.module('ui.directives').factory('keypressHelper', ['$parse', function keypress($parse){
+  var keysByCode = {
     8: 'backspace',
     9: 'tab',
     13: 'enter',
@@ -16,84 +16,87 @@ function keypress(mode, scope, elm, attrs, $parse){
     45: 'insert',
     46: 'delete'
   };
-  params = scope.$eval(attrs.uiKeypress);
+  
+  return function(mode, scope, elm, attrs) {
+    var params, combinations = [];
+    params = scope.$eval(attrs.uiKeypress);
 
-  // Prepare combinations for simple checking
-  angular.forEach(params, function (v, k) {
-    var combination = {};
-    combination.expression = $parse(v);
+    // Prepare combinations for simple checking
+    angular.forEach(params, function (v, k) {
+      var combination = {};
+      combination.expression = $parse(v);
 
-    angular.forEach(k.split(' '), function(variation) {
-      combination.keys = {};
-      angular.forEach(variation.split('-'), function (value) {
-        combination.keys[value] = true;
-      });
-      combinations.push(combination);
-    });
-  });
-
-  // Check only mathcing of pressed keys one of the conditions
-  elm.bind(mode, function (event) {
-    // No need to do that inside the cycle
-    var altPressed = event.metaKey || event.altKey;
-    var ctrlPressed = event.ctrlKey;
-    var shiftPressed = event.shiftKey;
-    var keyCode = event.keyCode;
-    
-    // normalize keycodes
-    if (mode === 'keypress' && !shiftPressed && keyCode >= 97 && keyCode <= 122) {
-      keyCode = keyCode - 32;
-    }
-
-    // Iterate over prepared combinations
-    angular.forEach(combinations, function (combination) {
-
-      var mainKeyPressed = (combination.keys[keysByCode[event.keyCode]] || combination.keys[event.keyCode.toString()]) || false;
-
-      var altRequired = combination.keys.alt || false;
-      var ctrlRequired = combination.keys.ctrl || false;
-      var shiftRequired = combination.keys.shift || false;
-
-      if (
-        mainKeyPressed &&
-        ( altRequired == altPressed ) &&
-        ( ctrlRequired == ctrlPressed ) &&
-        ( shiftRequired == shiftPressed )
-      ) {
-        // Run the function
-        scope.$apply(function () {
-          combination.expression(scope, { '$event': event });
+      angular.forEach(k.split(' '), function(variation) {
+        combination.keys = {};
+        angular.forEach(variation.split('-'), function (value) {
+          combination.keys[value] = true;
         });
-      }
+        combinations.push(combination);
+      });
     });
-  });
-}
+    // Check only mathcing of pressed keys one of the conditions
+    elm.bind(mode, function (event) {
+      // No need to do that inside the cycle
+      var altPressed = event.metaKey || event.altKey;
+      var ctrlPressed = event.ctrlKey;
+      var shiftPressed = event.shiftKey;
+      var keyCode = event.keyCode;
+
+      // normalize keycodes
+      if (mode === 'keypress' && !shiftPressed && keyCode >= 97 && keyCode <= 122) {
+        keyCode = keyCode - 32;
+      }
+
+      // Iterate over prepared combinations
+      angular.forEach(combinations, function (combination) {
+
+        var mainKeyPressed = (combination.keys[keysByCode[event.keyCode]] || combination.keys[event.keyCode.toString()]) || false;
+
+        var altRequired = combination.keys.alt || false;
+        var ctrlRequired = combination.keys.ctrl || false;
+        var shiftRequired = combination.keys.shift || false;
+
+        if (
+          mainKeyPressed &&
+          ( altRequired == altPressed ) &&
+          ( ctrlRequired == ctrlPressed ) &&
+          ( shiftRequired == shiftPressed )
+        ) {
+          // Run the function
+          scope.$apply(function () {
+            combination.expression(scope, { '$event': event });
+          });
+        }
+      });
+    });
+  };
+}]);
 
 /**
  * Bind one or more handlers to particular keys or their combination
  * @param hash {mixed} keyBindings Can be an object or string where keybinding expression of keys or keys combinations and AngularJS Exspressions are set. Object syntax: "{ keys1: expression1 [, keys2: expression2 [ , ... ]]}". String syntax: ""expression1 on keys1 [ and expression2 on keys2 [ and ... ]]"". Expression is an AngularJS Expression, and key(s) are dash-separated combinations of keys and modifiers (one or many, if any. Order does not matter). Supported modifiers are 'ctrl', 'shift', 'alt' and key can be used either via its keyCode (13 for Return) or name. Named keys are 'backspace', 'tab', 'enter', 'esc', 'space', 'pageup', 'pagedown', 'end', 'home', 'left', 'up', 'right', 'down', 'insert', 'delete'.
  * @example <input ui-keypress="{enter:'x = 1', 'ctrl-shift-space':'foo()', 'shift-13':'bar()'}" /> <input ui-keypress="foo = 2 on ctrl-13 and bar('hello') on shift-esc" />
  **/
-angular.module('ui.directives').directive('uiKeydown', ['$parse', function ($parse) {
+angular.module('ui.directives').directive('uiKeydown', ['keypressHelper', function(keypressHelper){
   return {
     link: function (scope, elm, attrs) {
-      keypress('keydown', scope, elm, attrs, $parse);
+      keypressHelper('keydown', scope, elm, attrs);
     }
   };
 }]);
 
-angular.module('ui.directives').directive('uiKeypress', ['$parse', function ($parse) {
+angular.module('ui.directives').directive('uiKeypress', ['keypressHelper', function(keypressHelper){
   return {
     link: function (scope, elm, attrs) {
-      keypress('keydown', scope, elm, attrs, $parse);
+      keypressHelper('keydown', scope, elm, attrs);
     }
   };
 }]);
 
-angular.module('ui.directives').directive('uiKeyup', ['$parse', function ($parse) {
+angular.module('ui.directives').directive('uiKeyup', ['keypressHelper', function(keypressHelper){
   return {
     link: function (scope, elm, attrs) {
-      keypress('keydown', scope, elm, attrs, $parse);
+      keypressHelper('keydown', scope, elm, attrs);
     }
   };
 }]);
