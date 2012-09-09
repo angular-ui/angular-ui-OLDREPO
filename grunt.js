ff395e0777a6c081cbe98e3568e0be81c9294aed
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-09-09 12:34:15 +0200

    Switch to testacular 0.2.0

--- grunt.js
+++ grunt.js
@@ -126,27 +126,24 @@ module.exports = function (grunt) {
   grunt.registerTask('server', 'start testacular server', function () {
     //Mark the task as async but never call done, so the server stays up
     var done = this.async();
-    testacular.server.start('test/test-config.js');
+    testacular.server.start({configFile:'test/test-config.js'});
   });
 
   grunt.registerTask('test', 'run tests (make sure server task is run first)', function () {
+
+    var testCmd = process.platform === 'win32' ? 'testacular.cmd' : 'testacular';
+    var testArgs = process.env.TRAVIS ? ['start', '--single-run', '--no-auto-watch', '--reporter=dots', '--browsers=Firefox'] : ['run'];
+
     var done = this.async();
-    grunt.utils.spawn({
-      cmd: process.platform === 'win32' ? 'testacular-run.cmd' : 'testacular-run',
-      args: ['test/test-config.js']
-    }, function (error, result, code) {
-      if (error) {
-        grunt.warn("Make sure the testacular server is online: run `grunt server`.\n" +
-          "Also make sure you have a browser open to http://localhost:8080/.\n" +
-          error.stdout + error.stderr);
-        //the testacular runner somehow modifies the files if it errors(??).
-        //this causes grunt's watch task to re-fire itself constantly,
-        //unless we wait for a sec
-        setTimeout(done, 1000);
+    var child = grunt.utils.spawn({cmd:testCmd, args:testArgs}, function (err, result, code) {
+      if (code) {
+        grunt.fail.fatal("Test failed...", code);
       } else {
-        grunt.log.write(result.stdout);
         done();
       }
     });
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
   });
 };
