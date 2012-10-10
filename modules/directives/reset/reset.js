f7b222f8d674088d02f532c431cae175e55ae645
Kyle Brown <blackbarn@gmail.com>
2012-10-09 22:59:53 -0700

    reset now supports defining of a reset value instead of being forced to use null

--- modules/directives/reset/reset.js
+++ modules/directives/reset/reset.js
@@ -5,14 +5,22 @@ angular.module('ui.directives').directive('uiReset', ['$parse', function ($parse
   return {
     require: 'ngModel',
     link: function (scope, elm, attrs, ctrl) {
-      var aElement = angular.element('<a class="ui-reset" />');
+      var opts, aElement;
+      opts = {};
+      if (attrs.uiReset) {
+        opts = scope.$eval(attrs.uiReset);
+        if (angular.isString(opts)) {
+          opts = {resetValue: opts};
+        }
+      }
+      opts = angular.extend({resetValue: null}, opts);
+      aElement = angular.element('<a class="ui-reset" />');
       elm.wrap('<span class="ui-resetwrap" />').after(aElement);
-
       aElement.bind('click', function (e) {
         e.preventDefault();
         scope.$apply(function () {
           // This lets you SET the value of the 'parsed' model
-          ctrl.$setViewValue(null);
+          ctrl.$setViewValue(opts.resetValue);
           ctrl.$render();
         });
       });
