99745df6029f74b90bb0280877bff818d6cd7daa
dandoyon <dandoyon@yahoo.com>
2012-10-28 14:38:17 -0700

    added doc to top

--- common/ieshiv/ieshiv.js
+++ common/ieshiv/ieshiv.js
@@ -1,5 +1,17 @@
 // READ: http://docs-next.angularjs.org/guide/ie
 // element tags are statically defined in order to accommodate lazy-loading whereby directives are also unknown
+
+// The ieshiv takes care of our ui.directives and AngularJS's ng-view, ng-include, ng-pluralize, ng-switch.
+// However, IF you have custom directives that can be used as html tags (yours or someone else's) then
+// add list of directives into <code>window.myCustomTags</code>
+
+// <!--[if lte IE 8]>
+//    <script>
+//    window.myCustomTags = [ 'yourCustomDirective', 'somebodyElsesDirective' ]; // optional
+//    </script>
+//    <script src="build/angular-ui-ieshiv.js"></script>
+// <![endif]-->
+
 (function (exports) {
 
   var debug = window.ieShivDebug || false,
