1c6459e6dedde628f7ed74e304992d21a6a014b8
Gareth James <garethdjames@gmail.com>
2012-12-06 10:02:23 +0000

    updated after code review of pull request

--- modules/filters/highlight/highlight.js
+++ modules/filters/highlight/highlight.js
@@ -7,41 +7,17 @@
  */
 angular.module('ui.filters').filter('highlight', function () {
     return function (text, search, caseSensitive, capture) {
-        if(!capture) {
-            if (search || angular.isNumber(search)) {
-                text = text.toString();
-                search = search.toString();
-                if (caseSensitive) {
-                    return text.split(search).join('<span class="ui-match">' + search + '</span>');
-                } else {
-                    return text.replace(new RegExp(search, 'gi'), '<span class="ui-match">$&</span>');
-                }
-            } else {
-                return text;
-            }
-        } else {
+        if (search || angular.isNumber(search)) {
+            text = text.toString();
+            search = search.toString();
             try {
-                if (search || angular.isNumber(search)) {
-                    text = text.toString();
-                    search = search.toString();
-                    var match;
-                    if (caseSensitive) {
-                        match = new RegExp(search, 'g').exec(text);
-                    } else {
-                        match = new RegExp(search, 'gi').exec(text);
-                    }
-                    var firstPart = text.substr(0, text.indexOf(match[1]));
-                    var secondPart = '<span class="ui-match">' + match[1] + '</span>';
-                    var lastPart = text.substr(text.indexOf(match[1]) + match[1].length, text.length);
-
-                    return firstPart + secondPart + lastPart;
-
-                } else {
-                    return text;
-                }
-            } catch (Exception){
+                return text.replace(new RegExp(search, caseSensitive && 'g' || 'gi'), '<span class="ui-match">' + (capture && '$1'|| '$&') + '</span>');
+            }
+            catch (Exception) {
                 return text.toString();
             }
+        } else {
+            return text;
         }
     };
 });
