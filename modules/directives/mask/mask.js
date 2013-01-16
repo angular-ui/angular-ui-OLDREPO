1291e087f39d7394107bcfbf1b3885c305254013
getuliojr <gromao@gmail.com>
2013-01-16 19:09:46 -0200

    Update modules/directives/mask/mask.js
    This new version accepts a parameter to remove or not the mask as it is updated to the model and also make it more compatible with uiDate Controller

--- modules/directives/mask/mask.js
+++ modules/directives/mask/mask.js
@@ -1,18 +1,32 @@
 /*
  Attaches jquery-ui input mask onto input element
+   @param [ui-mask] {object} Options available: 
+   - mask: set the mask of the object
+   - removeMask: true or false, if false the model will retain the mask as it was typed by the user.
  */
-angular.module('ui.directives').directive('uiMask', [
-  function () {
+angular.module('ui.directives').directive('uiMask', ['ui.config',
+  function (uiConfig) {
     return {
       require:'ngModel',
       link:function ($scope, element, attrs, controller) {
-
+        //get options
+        var options = angular.extend({}, uiConfig.mask, $scope.$eval(attrs.uiMask));
+        
         /* We override the render method to run the jQuery mask plugin
          */
         controller.$render = function () {
           var value = controller.$viewValue || '';
           element.val(value);
-          element.mask($scope.$eval(attrs.uiMask));
+          //If a mask parameter was set as object, parse its value
+          if (options["mask"] != undefined) {
+              //Check if it is starting as a "'" and add it, since it is required by Jquery
+              if (options["mask"].indexOf("'") != 0){
+                  options["mask"] = "'" + options["mask"] + "'";
+              }
+              element.mask($scope.$eval(options.mask));
+          } else {
+              element.mask($scope.$eval(attrs.uiMask));
+          }
         };
 
         /* Add a parser that extracts the masked value into the model but only if the mask is valid
@@ -25,13 +39,20 @@ angular.module('ui.directives').directive('uiMask', [
           return isValid ? value : undefined;
         });
 
-        /* When keyup, update the view value
-         */
-        element.bind('keyup', function () {
-          $scope.$apply(function () {
-            controller.$setViewValue(element.mask());
-          });
-        });
+        /* When changed, update the view value but only if it is not been used with the directive uiDate
+           uiDate will parse as a date so it is better format for dates, instead of string.
+        */
+        if ((attrs["uiDate"] == undefined) && (attrs["uiDateFormat"] == undefined)) {
+            element.bind('change', function () {
+                $scope.$apply(function () {
+                    if ((options["removeMask"] != undefined) && (options["removeMask"] == false)) {
+                        controller.$setViewValue(element.val());
+                    } else {
+                        controller.$setViewValue(element.mask());
+                    }
+                });
+            });
+        }
       }
     };
   }
