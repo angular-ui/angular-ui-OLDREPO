7ba49d0dba6ea3c638936907a37e11b2212d842c
Mikoto Okumura <mikotookumura@gmail.com>
2013-01-22 00:11:20 +0900

    Implementing "input's type property CAN be changed"
    This directive enables to write an input tag with a variable type property like:
    
         <input type="{{model}}" ui-input2 />
    
    You can not write the following code without this directive:
    
        <input type="{{model}}" />
    
    because jQuery throws an error: "type property can't be changed" when an type property is dynamically changed.
    This is due to an IE problem: http://stackoverflow.com/questions/1544317/jquery-change-type-of-input-field

---
+++ modules/directives/input2/input2.js
@@ -0,0 +1,33 @@
+/*
+ * Defines the ui-input2 tag, attribute and class(restrict ECA). This allow an input property to be changed
+ */
+angular.module('ui.directives').directive('uiInput2', ['$compile', function($compile) {
+  return {
+    restrict: 'ECA',
+    replace: true,
+    template: '<span />',
+    link: function(scope, iElement, iAttrs, controller) {
+      var clone = (function() {
+        var cursor = iElement;
+        return function(type) {
+          var input = angular.element('<input />');
+          angular.forEach(iAttrs.$attr, function(v,k) {
+            input.attr(v, iAttrs[k]);
+          });
+          input.removeAttr('ui-input2');
+          input.attr('type', type);
+          cursor.after(input);
+          cursor.remove();
+          cursor = input;
+          return input;
+        };
+      })();
+
+      clone('text');
+      iAttrs.$observe('type', function(type) {
+        var input = clone(type || 'text');
+        $compile(input)(scope);
+      });
+    }
+  };
+}]);
