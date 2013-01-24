1e89e6987aa4943c97a8d91a8481aea169148d57
Dean Sofer <dsofer@paxata.com>
2013-01-24 15:26:33 -0800

    fix(route) working on fleshing out tests and refactoring structure

--- modules/directives/route/route.js
+++ modules/directives/route/route.js
@@ -1,24 +1,41 @@
 /**
- * Add a clear button to form inputs to reset their value
+ * Set a $activeRoute boolean to see if the current route matches
  */
 angular.module('ui.directives').directive('uiRoute', ['ui.config', '$location', function (uiConfig, $location) {
-  if (uiConfig.route !== undefined)
-    routeClass = uiConfig.route;
-  else
-    routeClass = 'ui-active';
   return {
+    restrict: 'AC',
+    priority: 9999,
     link: function ($scope, elm, attrs) {
-      var watcher;
-      function checkRoute() {
-        var regexp = new RegExp('^' + watcher + '$', ["i"]);
-        elm.toggleClass(routeClass, regexp.test($location.path()));
+      var watcher = angular.noop;
+
+      // Used by href and ngHref
+      function observeHref(newVal) {
+        watcher = function watchHref() {
+          $scope.$routeActive = ($location.path().indexOf(newVal) > -1);
+        };
+        watcher();
       }
-      attrs.$observe('uiRoute', function(newVal) {
-        watcher = newVal;
-        checkRoute();
-      });
+
+      if (attrs.uiRoute) {
+        attrs.$observe('uiRoute', function(newVal) {
+          watcher = function watchRegex() {
+            var regexp = new RegExp('^' + watcher + '$', ["i"]);
+            $scope.$routeActive = regexp.test($location.path());
+          };
+          watcher();
+        });
+      } else if (attrs.ngHref) {
+        // Setup watcher() every time ngHref changes
+        attrs.$observe('ngHref', observeHref);
+      } else if (attrs.href) {
+        // Setup watcher()
+        observeHref(attrs.href);
+      } else {
+        throw new Error('uiRoute missing a route or href property on ' + elm[0]);
+      }
+
       $scope.$on('$routeChangeSuccess', function(){
-        checkRoute();
+        watcher();
       });
     }
   };
--- modules/directives/route/test/routeSpec.js
+++ modules/directives/route/test/routeSpec.js
@@ -1,24 +1,77 @@
 /*global describe, beforeEach, module, inject, it, spyOn, expect, $ */
-describe('uiReset', function () {
+describe('uiRoute', function () {
   'use strict';
 
-  var scope, $compile;
+  var scope, $compile, $location;
   beforeEach(module('ui.directives'));
-  beforeEach(inject(function (_$rootScope_, _$compile_, _$window_) {
+  beforeEach(inject(function (_$rootScope_, _$compile_, _$window_, _$location_) {
     scope = _$rootScope_.$new();
     $compile = _$compile_;
+    $location = _$location_;
   }));
 
-  describe('class toggle', function () {
-    it('should be triggered by changing interpolation value', function () {
+  describe('with uiRoute defined', function(){
+    it('should use the uiRoute property', function(){
+      $compile('<div ui-route="/foo" />')(scope);
     });
-    it('should be triggered by changing routes', function () {
+    it('should update $activeRoute on $observe', function(){
+      $location.path('/bar');
+      scope.$apply('foobar = "foo"');
+      $compile('<div ui-route="/{{foobar}}" />')(scope);
+      expect(scope.$activeRoute).toBeFalsy();
+      scope.$apply('foobar = "bar"');
+      expect(scope.$activeRoute).toBe(true);
+      scope.$apply('foobar = "foo"');
+      expect(scope.$activeRoute).toBe(false);
+    });
+    it('should support regular expression', function(){
+      $location.path('/foo/123');
+      $compile('<div ui-route="/foo/[0-9]*" />')(scope);
+      expect(scope.$activeRoute).toBe(true);
     });
   });
-  describe('element class', function () {
-    it('should be "ui-active" by default', function () {
+
+  describe('with ngHref defined', function(){
+
+    it('should use the ngHref property', function(){
+      $location.path('/foo');
+      $compile('<a ng-href="/foo" ui-route />')(scope);
+      expect(scope.$activeRoute).toBe(true);
     });
-    it('should value set in uiConfig', function () {
+    it('should update $activeRoute on $observe', function(){
+      $location.path('/bar');
+      scope.$apply('foobar = "foo"');
+      $compile('<a ng-href="/{{foobar}}" ui-route />')(scope);
+      expect(scope.$activeRoute).toBeFalsy();
+      scope.$apply('foobar = "bar"');
+      expect(scope.$activeRoute).toBe(true);
+      scope.$apply('foobar = "foo"');
+      expect(scope.$activeRoute).toBe(false);
     });
+  });
+
+  describe('with href defined', function(){
+
+    it('should use the href property', function(){
+      $location.path('/foo');
+      $compile('<a href="/foo" ui-route />')(scope);
+      expect(scope.$activeRoute).toBe(true);
+    });
+  });
+
+  it('should throw an error if no route property available', function(){
+    expect(function(){
+      $compile('<div ui-route/>')(scope);
+    }).toThrow();
+  });
+
+  it('should update $activeRoute on route change', function(){
+    $location.path('/bar');
+    $compile('<div ui-route="/foo" />')(scope);
+    expect(scope.$activeRoute).toBeFalsy();
+    $location.path('/foo');
+    expect(scope.$activeRoute).toBe(true);
+    $location.path('/bar');
+    expect(scope.$activeRoute).toBe(false);
   });
 });
