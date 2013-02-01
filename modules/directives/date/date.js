5460234fbb9392415ac2c1b1689054228aa576db
Dean Sofer <dsofer@paxata.com>
2013-02-01 01:52:21 -0800

    feat(dateFormat): Added support for ui.config closes #349

--- modules/directives/date/date.js
+++ modules/directives/date/date.js
@@ -75,23 +75,12 @@ angular.module('ui.directives')
 }
 ])
 
-.directive('uiDateFormat', [function() {
+.directive('uiDateFormat', ['ui.config', function(uiConfig) {
   var directive = {
     require:'ngModel',
     link: function(scope, element, attrs, modelCtrl) {
-      if ( attrs.uiDateFormat === '' ) {
-        // Default to ISO formatting
-        modelCtrl.$formatters.push(function(value) {
-          if (angular.isString(value) ) {
-            return new Date(value);
-          }
-        });
-        modelCtrl.$parsers.push(function(value){
-          if (value) {
-            return value.toISOString();
-          }
-        });
-      } else {
+      var dateFormat = attrs.uiDateFormat || uiConfig.dateFormat;
+      if ( dateFormat ) {
         var format = attrs.uiDateFormat;
         // Use the datepicker with the attribute value as the format string to convert to and from a string
         modelCtrl.$formatters.push(function(value) {
@@ -102,6 +91,18 @@ angular.module('ui.directives')
         modelCtrl.$parsers.push(function(value){
           if (value) {
             return $.datepicker.formatDate(format, value);
+          }
+        });
+      } else {
+        // Default to ISO formatting
+        modelCtrl.$formatters.push(function(value) {
+          if (angular.isString(value) ) {
+            return new Date(value);
+          }
+        });
+        modelCtrl.$parsers.push(function(value){
+          if (value) {
+            return value.toISOString();
           }
         });
       }
