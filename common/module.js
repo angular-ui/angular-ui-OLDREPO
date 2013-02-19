20f2c2a316d3079377234799cf9b650c6aac550c
Andy Joslin <andytjoslin@gmail.com>
2013-02-19 10:49:46 -0500

    feat(promiseTracker): add promiseTracker service
    Allows tracking of http requests or any promise.

--- common/module.js
+++ common/module.js
@@ -2,4 +2,5 @@
 angular.module('ui.config', []).value('ui.config', {});
 angular.module('ui.filters', ['ui.config']);
 angular.module('ui.directives', ['ui.config']);
-angular.module('ui', ['ui.filters', 'ui.directives', 'ui.config']);
+angular.module('ui.services', ['ui.config']);
+angular.module('ui', ['ui.filters', 'ui.directives', 'ui.services', 'ui.config']);
