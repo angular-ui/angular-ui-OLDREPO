780f60b51f8edc5d300fb6326a5daa83a61d0a6d
Anatoly Mironov <mirontoli@gmail.com>
2013-02-23 23:54:05 +0100

    Add initial tests for uiAutocomplete
    Include them into the Pull Request #414

--- modules/directives/autocomplete/test/autocompleteSpec.js
+++ modules/directives/autocomplete/test/autocompleteSpec.js
@@ -1,5 +1,27 @@
 /*global describe, beforeEach, it, inject, expect, module, $*/
-describe('uiDate', function() {
+describe('uiAutocomplete', function() {
   'use strict';
-  //to be added
+  var source = ["javascript", "ruby", "c#", "haskell"];
+  var options = {
+	source: source
+  }
+  beforeEach(module('ui.directives'));
+  describe('simple use on input element', function() {
+    it('should have an autocomplete attached', function() {
+      inject(function($compile, $rootScope) {
+        var element;
+        element = $compile("<input ui-autocomplete/>")($rootScope);
+        expect(element.autocomplete()).toBeDefined();
+      });
+    });
+	it('should update autocomplete term when the element value is changed', function() {
+      inject(function($compile, $rootScope) {
+        var term, element;
+		term = "j";
+        element = $compile("<input ui-autocomplete='{\"source\": [\"hej\"]}' ng-model='x'/>")($rootScope);
+		element.val(term);
+        expect(element.data("autocomplete").term).toEqual(term);
+      });
+    });
+  });
 });
