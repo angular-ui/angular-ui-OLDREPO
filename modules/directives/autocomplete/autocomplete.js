a8607dc7c412a404e2c6a604ffe775ff95267b81
Jakob <jsmith@incognito.com>
2013-03-26 11:57:58 -0700

    Added wrapper for jQuery UI Autcomplete plugin (known bug in wrapper: selecting a suggestion via mouseclick does not update model, suspect problem with jQuery UI.)

---
+++ modules/directives/autocomplete/autocomplete.js
@@ -0,0 +1,62 @@
+/*global angular */
+/*
+jQuery UI Autocomplete plugin wrapper
+
+@param [ui-autocomplete] {object} Options to pass to $.fn.autocomplete() merged onto ui.config
+*/
+
+angular.module('ui.directives')
+
+.directive('uiAutocomplete',
+  [ 'ui.config',
+    function (uiConfig)
+    {
+      'use strict';
+      var options = {};
+      if ( angular.isObject(uiConfig.autocomplete) )
+      {
+        angular.extend(options, uiConfig.autocomplete);
+      }
+      return {
+        require: '?ngModel',
+        link: function (scope, element, attrs, controller)
+        {
+          var getOptions = function ()
+          {
+            var options = angular.extend({}, uiConfig.autocomplete, scope.$eval(attrs.uiAutocomplete));
+            return options;
+          };
+
+          var initAutocomplete = function ()
+          {
+            var opts = getOptions();
+
+            // If we have a controller (i.e. ngModelController) then wire it up
+            if ( controller )
+            {
+              var updateModel = function () {
+                if ( !scope.$$phase )
+                {
+                  scope.$apply(function () {
+                    var query = element.val();
+                    controller.$setViewValue(query);
+                    element.blur();
+                  });
+                }
+              }
+              
+              opts.select = updateModel;
+              // In case the user changes the text directly in the input box
+              element.bind('autocompleteselect', updateModel);
+            }
+            // Create the new autocomplete widget
+            element.autocomplete(opts);
+          };
+          // Watch for changes to the directives options
+          scope.$watch(getOptions, initAutocomplete, true);
+        }
+      };
+    }
+  ]
+);
+
